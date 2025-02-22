e24fb8040382309654b527f583231be1045b4546
Sarthak Singh Tariyal <sarthaksinghtariyal@gmail.com>
2025-02-23 01:26:07 +0530

    changed images

--- frontend/medi_chain/components/landing/Footer.tsx
+++ frontend/medi_chain/components/landing/Footer.tsx
@@ -1,10 +1,10 @@
-'use client';
+"use client";
 import React, { useEffect, useState } from "react";
 import { motion, useMotionValue } from "framer-motion";
 import LandingPage from "@/components/landing/LandingPage";
 import IssueTrackerPage from "@/components/landing/Navbar";
 import GlowingEffectDemo from "@/components/landing/box/box1";
-import End from "@/components/landing/End"
+import End from "@/components/landing/End";
 import { Merriweather, Montserrat } from "next/font/google";
 const merriweather = Merriweather({
   subsets: ["latin"],
@@ -17,10 +17,10 @@ const montserrat = Montserrat({
 });
 
 const imgs = [
-  "https://stgaccinwbsdevlrs01.blob.core.windows.net/newcorporatewbsite/homepage-banners/January2024/CCeVjCKszDYpK1Zw5MPj.webp?w=3840&q=75 ",
+  "https://stgaccinwbsdevlrs01.blob.core.windows.net/newcorporatewbsite/homepage-banners/January2024/CCeVjCKszDYpK1Zw5MPj.webp?w=3840&q=75",
   "https://stgaccinwbsdevlrs01.blob.core.windows.net/newcorporatewbsite/homepage-banners/January2024/VN8DnbFlModceGUJm44g.webp?w=3840&q=75 ",
-  "https://img.freepik.com/free-vector/hospital-team-diverse-healthcare-staff-doctors_107791-11611.jpg?ga=GA1.1.166189983.1736508498&semt=ais_hybrid",
-  "https://img.freepik.com/premium-photo/portrait-male-doctor-with-stethoscope-arm-cross-isolated-white-wall-health-insurance-concept_3544-1512.jpg"];
+  "https://stgaccinwbsdevlrs01.blob.core.windows.net/newcorporatewbsite/homepage-banners/February2025/3awSWYmdv3ShIr1e9ViZ.webp?w=3840&q=75",
+];
 
 const AUTO_DELAY = 6000;
 const DRAG_BUFFER = 50;
@@ -56,9 +56,9 @@ const Images = () => {
 
   return (
     <div>
-        <div>
-            <IssueTrackerPage/>
-        </div>
+      <div>
+        <IssueTrackerPage />
+      </div>
       <div className="relative overflow-hidden">
         {/* Image Carousel */}
         <motion.div
@@ -78,7 +78,7 @@ const Images = () => {
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
-              animate={{ scale: imgIndex === idx ? 1.0 : 1.0  }}
+              animate={{ scale: imgIndex === idx ? 1.0 : 1.0 }}
               transition={SPRING_OPTIONS}
               className="w-screen shrink-0 bg-neutral-800"
             >
@@ -106,11 +106,16 @@ const Images = () => {
         <div className="pointer-events-none absolute bottom-0 right-0 top-0 w-[10vw] max-w-[100px] bg-gradient-to-l from-neutral-950/50 to-neutral-950/0" />
       </div>
       <LandingPage />
-      <div className={`${merriweather.className}  text-center p-5 text-7xl font-semibold  whitespace-nowrap text-black font-serif`}> Our Services </div>
-      <div className="p-12 mx-16 my-10"> 
-        <GlowingEffectDemo/>
+      <div
+        className={`${merriweather.className}  text-center p-5 text-7xl font-semibold  whitespace-nowrap text-black font-serif`}
+      >
+        {" "}
+        Our Services{" "}
       </div>
-      <End/>
+      <div className="p-12 mx-16 my-10">
+        <GlowingEffectDemo />
+      </div>
+      <End />
     </div>
   );
 };
